1fa8b6b0e7017aa49e7b895e13180dffd228cd7c
user <642324358@qq.com>
2015-04-02 15:34:48 +0800

    msv

---
+++ MSV/_MSVInput.m
@@ -0,0 +1,25 @@
+{int x<==3 and skip;int y<==4 and skip, 
+int m<==5 and skip;int n<==6 and skip
+}
+prj
+{int z<==4 and skip};
+if(x<4)
+then
+{
+ int p<==3 and skip
+}
+else
+{
+ int q<==4 and skip
+}
+
+
+ 
+
+
+
+
+
+
+
+
